3a269521a3b50ca576086c09cde6677225fc00ab
SamIfesi <Ekesamrose@gmail.com>
2026-09-21 21:43:59 +0100

    Remove migration script for token_version column in users table

--- database/migration_token_version.sql
+++
@@ -1,9 +0,0 @@
--- Adds token_version, used to invalidate JWTs on other devices when a
--- user changes their password (see JWTService, AuthMiddleware,
--- ProfileController::changePassword, AuthController::resetPassword).
---
--- Run this once against the existing production/dev database:
---   mysql -u <user> -p <database> < migration_token_version.sql
-
-ALTER TABLE `users`
-  ADD COLUMN `token_version` int unsigned NOT NULL DEFAULT '0' AFTER `password_hash`;
